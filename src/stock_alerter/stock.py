6f75ad1a7491d1948d218c8046678e27bdbcf34f
nbnguyen1997 <binhnguyen2114@gmail.com>
2021-03-16 22:10:14 +0700

    [create] Reorganizing the test code

--- src/stock_alerter/stock.py
+++ src/stock_alerter/stock.py
@@ -5,10 +5,6 @@ class Stock():
         self.symbol = symbol
         self.price = None
 
-class StockTest(unittest.TestCase):
-    def test_price_of_a_new_stock_class_should_be_None(self):
-        stock = Stock("GOOG")
-        self.assertIsNone(stock.price)
 
 if __name__ == "__main__":
     unittest.main()
