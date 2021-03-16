4f386c5b57854ed9e67836db61b657bee7841c68
nbnguyen1997 <binhnguyen2114@gmail.com>
2021-03-16 22:46:39 +0700

    [modified] Arrange-Act-Assert

--- src/stock_alerter/tests/test_stock.py
+++ src/stock_alerter/tests/test_stock.py
@@ -1,7 +1,13 @@
 import unittest
+import datetime
 from ..stock import Stock
 
 class StockTest(unittest.TestCase):
     def test_price_of_a_new_stock_class_should_be_None(self):
         stock = Stock("GOOG")
         self.assertIsNone(stock.price)
+    
+    def test_stock_update(self):
+        goog = Stock("GOOG")
+        goog.update(datetime(2021,3,16),price=10)
+        self.assertEqual(10,goog.price)
