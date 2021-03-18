ed2e9445be45867ef3217c473879b9cb226979c1
nbnguyen1997 <binhnguyen2114@gmail.com>
2021-03-19 02:15:34 +0700

    [modified] smells code

--- src/stock_alerter/stock.py
+++ src/stock_alerter/stock.py
@@ -1,21 +1,46 @@
 import bisect
 import collections
-PriceEvent = collections.namedtuple("PriceEvent",["timestamp","price"])
+from datetime import timedelta
+PriceEvent = collections.namedtuple("PriceEvent", ["timestamp", "price"])
+
+
 class Stock:
-    def __init__(self,symbol):
+    def __init__(self, symbol):
         self.symbol = symbol
         self.price_history = []
 
     def update(self, timestamp, price):
         if price < 0:
             raise ValueError("price should not be negative")
-        
-        bisect.insort_left(self.price_history,PriceEvent(timestamp, price))
-    
+
+        bisect.insort_left(self.price_history, PriceEvent(timestamp, price))
+
     def is_increasing_trend(self):
         return self.price_history[-3].price < self.price_history[-2][1] < self.price_history[-1][1]
 
+    def get_crossover_signal(self, on_date):
+        cpl = []
+        for i in range(11):
+            chk = on_date.date()-timedelta()
+            for price_event in reversed(self.price_history):
+                if price_event.timestamp.date() > chk:
+                    pass
+                if price_event.timestamp.date() == chk:
+                    cpl.insert(0, price_event)
+                    break
+                if price_event.timestamp.date() < chk:
+                    cpl.insert(0, price_event)
+        # Return NEUTRAL signal
+        if len(cpl) < 11:
+            return 0
+
+        #   BUY signal
+        if sum([update.price for update in cpl[-11:-1]])/10 > sum([update.price for update in cpl[-6, -1]])/5 < sum([update.price for update in cpl[-5:]])/5:
+            return 1
+
+        #NEUTRAL signal
+        return 0
+
     @property
     def price(self):
-        return self.price_history[-1].price  if self.price_history else None
-
+        return self.price_history[-1].price if self.price_history else None
