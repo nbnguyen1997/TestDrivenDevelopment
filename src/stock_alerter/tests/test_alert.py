1009ed9dffb13f52417122c398ef63e0785e5a22
nbnguyen1997 <binhnguyen2114@gmail.com>
2021-03-23 21:56:08 +0700

    mock the Stock

--- src/stock_alerter/tests/test_alert.py
+++ src/stock_alerter/tests/test_alert.py
@@ -15,12 +15,24 @@ class TestAction:
 
 class AlertTest(unittest.TestCase):
     def tets_action_is_executed_when_rule_matches(self):
-        exchange = {"GOOG":Stock("GOOG")}
+        # exchange = {"GOOG":Stock("GOOG")}
+        # rule = mock.MagicMock(spec=PriceRule)
+        # rule.matches.return_value = True
+        # rule.depends_on.return_value={"GOOG"}
+        # action = mock.MagicMock()
+        # alert=Alert("sample alert",rule,action)
+        # alert.connect(exchange)
+        # exchange["GOOG"].update(datetime(2014,2,10),11)
+        # action.execute.assert_called_with("sample alert")
+        goog = mock.MagicMock(spec=Stock)
+        goog.updated = Event()
+        goog.update.side_effect = lambda date,value: goog.updated.fire(self)
+        exchange = {"GOOG": goog}
         rule = mock.MagicMock(spec=PriceRule)
         rule.matches.return_value = True
-        rule.depends_on.return_value={"GOOG"}
+        rule.depends_on.return_value ={"GOOG"}
         action = mock.MagicMock()
-        alert=Alert("sample alert",rule,action)
+        alert = Alert("sample alert",rule,action)
         alert.connect(exchange)
         exchange["GOOG"].update(datetime(2014,2,10),11)
-        action.execute.assert_called_with("sample alert")
+        action.excute.assert_called_with("sample alert")
