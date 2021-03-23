8b85f459f7b4b45da36cffaab7c53c81540acf5e
nbnguyen1997 <binhnguyen2114@gmail.com>
2021-03-23 23:29:54 +0700

    Using method decorator

--- src/stock_alerter/tests/test_action.py
+++ src/stock_alerter/tests/test_action.py
@@ -3,9 +3,10 @@ import unittest
 from unittest import mock
 from ..action import PrintAction
 
+
 class PrintActionTest(unittest.TestCase):
-    def test_executing_action_prints_message(self):
-        with mock.patch("builtins.print") as mock_print:
-            action = PrintAction()
-            action.execute("GOOG > $10")
-            mock_print.assert_called_with("GOOG > $10")
+    @mock.patch("builtins.print")
+    def test_executing_action_prints_message(self,mock_print):    
+        action = PrintAction()
+        action.execute("GOOG > $10")
+        mock_print.assert_called_with("GOOG > $10")
