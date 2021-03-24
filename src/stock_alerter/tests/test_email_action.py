914d79f13b8bc58e5f630134b4fb930afa633b12
nbnguyen1997 <binhnguyen2114@gmail.com>
2021-03-24 20:23:06 +0700

    test connection closed after sending mail

--- src/stock_alerter/tests/test_email_action.py
+++ src/stock_alerter/tests/test_email_action.py
@@ -11,3 +11,12 @@ class EmailActionTest(unittest.TestCase):
     def test_email_is_send_to_the_right_ser(self,mock_smtp_class):
         self.action.execute("MSFT has crossed $10 price level")
         mock_smtp_class.assert_called_with("email.stocks.com")
+
+    def test_connection_closed_after_sending_mail(self,mock_smtp_class):
+        mock_smtp =mock_smtp_class.return_value
+        self.action.execute("MSFT has crossed $10 price level")
+        mock_smtp.send_message.assert_called_with(mock.ANY)
+        self.assertTrue(mock_smtp.quit.called)
+        mock_smtp.assert_has_calls([
+            mock.call.send_message(mock.ANY),
+            mock.call.quit()])
