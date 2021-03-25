2fb121a8da4f1c0f9440243d380b269ca1fe4a9d
nbnguyen1997 <binhnguyen2114@gmail.com>
2021-03-25 21:50:36 +0700

    fix ImportErrror stock and legacy

--- src/stock_alerter/legacy.py
+++ src/stock_alerter/legacy.py
@@ -1,14 +1,15 @@
 from datetime import datetime
-from .stock import Stock
-from .rule import PriceRule
+import stock
+# from .rule import PriceRule
+import rule
 
 
 class AlertProcessor:
     def __init__(self):
-        self.exchange = {"GOOG": Stock("GOOG"),
-                         "AAPL": Stock("AAPL")}
-        rule_1 = PriceRule("GOOG", lambda stock: stock.price > 10)
-        rule_2 = PriceRule("AAPL", lambda stock: stock.price > 5)
+        self.exchange = {"GOOG": sotck.Stock("GOOG"),
+                         "AAPL": stock.Stock("AAPL")}
+        rule_1 = rule.PriceRule("GOOG", lambda stock: stock.price > 10)
+        rule_2 = rule.PriceRule("AAPL", lambda stock: stock.price > 5)
         # self.exchange["GOOG"].update.connect(
         #     lambda stock: print(stock.symbol, stock.price)
         #     if rule_1.matches(self.exchange) else None)
@@ -18,7 +19,7 @@ class AlertProcessor:
 
         updates = []
         
-        with open(r"stock_alerter\updates.csv", "r") as fp:
+        with open(r".\updates.csv", "r") as fp:
             for line in fp.readline():
                 symbol, timestamp, price = line.strip().split(',')
                 updates.append((symbol, datetime.strptime(
