51f11a780696d31d1072c0c0b846530b4cbe7b2b
nbnguyen1997 <binhnguyen2114@gmail.com>
2021-03-26 22:41:17 +0700

    ImportErroe file test_legacy.py

--- src/stock_alerter/legacy.py
+++ src/stock_alerter/legacy.py
@@ -5,31 +5,32 @@ from .rule import PriceRule
 
 
 class AlertProcessor:
-    def __init__(self):
-        self.exchange = {"GOOG": Stock("GOOG"),"AAPL": Stock("AAPL")}
-        rule_1 = PriceRule("GOOG", lambda stock: stock.price > 10)
-        rule_2 = PriceRule("AAPL", lambda stock: stock.price > 5)
-        self.exchange["GOOG"].updated.connect(lambda stock: print(stock.symbol, stock.price) if rule_1.matches(self.exchange) else None)
-        self.exchange["AAPL"].updated.connect(lambda stock: print(stock.symbol, stock.price) if rule_2.matches(self.exchange) else None)
-
-    # def __init__(self, exchange=None):
-    #     if exchange is None:
-    #         self.exchange = {"GOOG": Stock("GOOG"), "AAPL": Stock("AAPL")}
-    #     else:
-    #         self.exchange = exchange
+    # def __init__(self):
+    #     self.exchange = {"GOOG": Stock("GOOG"),"AAPL": Stock("AAPL")}
     #     rule_1 = PriceRule("GOOG", lambda stock: stock.price > 10)
     #     rule_2 = PriceRule("AAPL", lambda stock: stock.price > 5)
-    #     self.exchange["GOOG"].updated.connect(lambda stock: self.print_action(stock, rule_1))
-    #     self.exchange["AAPL"].updated.connect(lambda stock: self.print_action(stock, rule_2))
+    #     self.exchange["GOOG"].updated.connect(lambda stock: print(stock.symbol, stock.price) if rule_1.matches(self.exchange) else None)
+    #     self.exchange["AAPL"].updated.connect(lambda stock: print(stock.symbol, stock.price) if rule_2.matches(self.exchange) else None)
+
+    def __init__(self,autorun=True,reader=None, exchange=None):
+        if exchange is None:
+            self.exchange = {"GOOG": Stock("GOOG"), "AAPL": Stock("AAPL")}
+        else:
+            self.exchange = exchange
+        rule_1 = PriceRule("GOOG", lambda stock: stock.price > 10)
+        rule_2 = PriceRule("AAPL", lambda stock: stock.price > 5)
+        # self.exchange["GOOG"].updated.connect(lambda stock: self.print_action(stock, rule_1))
+        # self.exchange["AAPL"].updated.connect(lambda stock: self.print_action(stock, rule_2))
+        self.exchange["GOOG"].updated.connect(lambda stock: print(stock.symbol, stock.price) if rule_1.matches(self.exchange) else None)
+        self.exchange["AAPL"].updated.connect(lambda stock: print(stock.symbol, stock.price) if rule_2.matches(self.exchange) else None)
         updates = []
-        
+
         with open("updates.csv", "r") as fp:
             for line in fp.readlines():
                 symbol, timestamp, price = (line.strip().split(','))
                 self.exchange[symbol].updated.connect(lambda stock: print(stock.symbol, stock.price) if rule_1.matches(self.exchange) else None)
                 self.exchange[symbol].updated.connect(lambda stock: print(stock.symbol, stock.price) if rule_2.matches(self.exchange) else None)
-                updates.append((symbol, datetime.strptime(
-                    timestamp, "%Y-%m-%dT%H:%M:%S.%f"), int(price)))
+                updates.append((symbol, datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f"), int(price)))
 
         for symbol, timestamp, price in updates:
 
