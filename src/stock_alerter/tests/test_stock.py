5d5f0175f1f0c533c0e255f7a89df3941106a4e4
nbnguyen1997 <binhnguyen2114@gmail.com>
2021-04-03 20:31:48 +0700

    Parameterized tests

--- src/stock_alerter/tests/test_stock.py
+++ src/stock_alerter/tests/test_stock.py
@@ -1,17 +1,48 @@
 import unittest
 import collections
+from nose2.tools.params import params
 from datetime import datetime, timedelta
 from ..stock import Stock, StockSignal
 
 
+
+def setup_test():
+    global goog
+    goog = Stock("GOOG")
+
+def teardown_test():
+    global goog
+    goog = None
+    
+def test_price_of_a_new_stock_class_should_be_None():
+    assert goog.price is None
+test_price_of_a_new_stock_class_should_be_None.setup = setup_test
+test_price_of_a_new_stock_class_should_be_None.teardown = teardown_test
+
+def given_a_series_of_prices(stock, prices):
+        timestamps = [datetime(2014, 2, 10), 
+                      datetime(2014, 2,11), 
+                      datetime(2014, 2, 12), 
+                      datetime(2014, 2, 13)]
+        for timestamp, price in zip(timestamps, prices):
+            stock.update(timestamp, price)
+@params(
+    ([8,10,12],True),
+    ([8,12,10],False),
+    ([8,10,10],False)
+)
+
+def test_stock_trends(prices,expected_output):
+    goog = Stock("GOOG")
+    given_a_series_of_prices(goog,prices)
+    assert goog.is_increasing_trend() == expected_output
 class StockTest(unittest.TestCase):
     @classmethod
     def setUp(self):
         self.goog = Stock("GOOG")
 
-    def test_price_of_a_new_stock_class_should_be_None():
-        goog = Stock("GOOG")
-        assert goog.price is None
+    def test_price_of_a_new_stock_class_should_be_None(self):
+        self.assertIsNone(self.goog.price)
 
     def test_stock_update(self):
         self.goog.update(datetime(2021, 3, 16), price=10)
@@ -33,7 +64,7 @@ class StockTest(unittest.TestCase):
 
 
 class StockTrendTest(unittest.TestCase):
-    @classmethod
+    
     def setUp(cls):
         cls.goog = Stock("GOOG")
 
